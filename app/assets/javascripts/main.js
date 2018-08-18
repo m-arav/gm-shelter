de09c1e178df935827dc82c71e9618b1e05d6033
Vimal V Nair <vimalvnair999@gmail.com>
2018-08-19 00:07:06 +0530

    remove 'let's

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,8 @@
 var map;
 var markers = [];
 var collectionCenterIcon = 'http://res.cloudinary.com/drch6exvq/image/upload/c_scale,h_32/v1534605020/Kerala%20Flood/collection.png'
-
+var shelterIcon = 'http://res.cloudinary.com/drch6exvq/image/upload/c_scale,h_32,q_100/v1534605020/Kerala%20Flood/shelter.png'
+var infowindow = null;
 function initMap() {
     map = new google.maps.Map(
 	document.getElementById('map'), {zoom: 12, center: {lat: 9.97892, lng: 76.3180348}});
@@ -14,30 +15,32 @@ function initMap() {
 }
 
 function addMarker(ldata) {
-    let nearCCButton = ''
-    let directionToCCButton = ''
+    nearCCButton = ''
+    directionToCCButton = ''
     if(ldata.facility_type != 'relief_material_collection'){
 	nearCCButton = 	'<input class="primary-btn" onclick="nearByCollectionCenters('+ ldata.location.lat + ',' + ldata.location.lon + ');" type=button value="Near by collection centers">';
 	directionToCCButton = '<input class="primary-btn" onclick="routeToNearestCC('+ ldata.location.lat + ',' + ldata.location.lon + ');" type=button value="Direction to nearest collection center">';
     }
-    let infoDetails = '<h3>' + ldata.name + '</h3>' +
+    infoDetails = '<h3>' + ldata.name + '</h3>' +
 	'<div id="address"> <b>Address : </b> ' + ldata.humanized_address + '</div><br>' +
 	'<div id="details"> <b>Contact : </b>' + ldata.contact + '</div><br>' +
         '<a href="' + 'https://www.google.com/maps/search/?api=1&query=' + ldata.location.lat + ',' + ldata.location.lon + '" target="_blank">Open in Google Maps</a><br><br>' +
 	nearCCButton + '<br><br>' + directionToCCButton;
 
-    let markerInfo = {location: {position: {lat: ldata.location.lat, lng: ldata.location.lon}, icon: null}, info: infoDetails, id: ldata.id, facilityType: ldata.facility_type};
+    markerInfo = {location: {position: {lat: ldata.location.lat, lng: ldata.location.lon}, icon: shelterIcon}, info: infoDetails, id: ldata.id, facilityType: ldata.facility_type};
     if(ldata.facility_type == 'relief_material_collection'){
 	markerInfo.location.icon = collectionCenterIcon;
     };
 
-    let marker = new google.maps.Marker(markerInfo.location);
+    marker = new google.maps.Marker(markerInfo.location);
     marker.set("id", markerInfo.id);
     infowindow = new google.maps.InfoWindow;
-    marker.addListener('click', function() {
-	infowindow.setContent(markerInfo.info);
-	infowindow.open(map, marker);
-    });
+    marker.addListener('click', (function(marker, map, markerInfo) {
+	return function(){
+	    infowindow.setContent(markerInfo.info);
+	    infowindow.open(map, marker);
+	};
+    })(marker, map, markerInfo));
 
     marker.setMap(map)
     markers.push(marker);
