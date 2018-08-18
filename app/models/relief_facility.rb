894eb1a475b0eceb789b7d5630feec3109171e7d
KP <kp2222@gmail.com>
2018-08-18 23:00:45 +0530

    sort by distance

--- app/models/relief_facility.rb
+++ app/models/relief_facility.rb
@@ -3,8 +3,8 @@ class ReliefFacility < ApplicationRecord
 
   scope :within, -> (latitude, longitude, distance_in_km = 5) {
     where(%{
-     ST_Distance(location, 'POINT(%f %f)') < %d
-    } % [longitude, latitude, distance_in_km * 5000]) # approx
+     ST_DWithin(location, 'POINT(%f %f)',%d)
+    } % [longitude, latitude, distance_in_km * 5000]).order("ST_Distance(location, 'POINT(#{longitude} #{latitude})') asc") # approx
   }
   scope :colleciton_centers, -> () { where(facility_type: "relief_material_collection") }
 
