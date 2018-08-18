84adef8fd67a3705ed4e746416cb4a109c700646
Aravind <thearavindmurali@gmail.com>
2018-08-18 21:48:24 +0530

    Add collection center scope for radial search

--- app/controllers/api/relief_facilities_controller.rb
+++ app/controllers/api/relief_facilities_controller.rb
@@ -2,7 +2,7 @@ class Api::ReliefFacilitiesController < ApplicationController
 
   def search
     @relief_facilities = ReliefFacility
-    @relief_facilities = @relief_facilities.within(*params[:within].split(",").collect(&:to_f)) if params[:within]
+    @relief_facilities = @relief_facilities.within(*params[:within].split(",").collect(&:to_f)).colleciton_centers if params[:within]
 
     render json: @relief_facilities.all
 
