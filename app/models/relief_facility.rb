f6f56e80b6712d811a34eb0ea924ab8145f889da
Vimal V Nair <vimalvnair999@gmail.com>
2018-08-18 23:14:59 +0530

    within 8 kms radius

--- app/models/relief_facility.rb
+++ app/models/relief_facility.rb
@@ -1,10 +1,10 @@
 class ReliefFacility < ApplicationRecord
   include ActiveModel::Serialization
 
-  scope :within, -> (latitude, longitude, distance_in_km = 5) {
+  scope :within, -> (latitude, longitude, distance_in_km = 8) {
     where(%{
      ST_DWithin(location, 'POINT(%f %f)',%d)
-    } % [longitude, latitude, distance_in_km * 5000]).order("ST_Distance(location, 'POINT(#{longitude} #{latitude})') asc") # approx
+    } % [longitude, latitude, distance_in_km * 1000]).order("ST_Distance(location, 'POINT(#{longitude} #{latitude})') asc") # approx
   }
   scope :colleciton_centers, -> () { where(facility_type: "relief_material_collection") }
 
